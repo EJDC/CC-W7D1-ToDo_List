ea790c1c0ce8b20b09bc7419df728e91b1b86ad3
EJDC <user@users-MacBook-Pro.local>
2022-11-28 16:34:05 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([
+      { name: "Buy shopping", priority: "high" },
+      { name: "Clean bathroom", priority: "low" },
+      { name: "Car's MOT", priority: "high" }
+  ])
+
+  const [newTask, setNewTask] = useState("");
+  const [priority, setPriority] = useState("");
+
+  const taskNodes = tasks.map((task, index) => {
+    return (
+      <li key={index} className={task.priority === "high" ? "high" : "low"}>
+        <span>{task.name}</span>
+        {task.priority === "high" ? <span className="high">High</span> : <span className="low">Low</span>}
+      </li>
+    )
+  })
+
+  const handleInputChange = (event) => {
+    setNewTask(event.target.value)
+  }
+
+  const saveNewTask = (event) => {
+    event.preventDefault();
+    // to test console.log(event.target.priority.value)
+    const newTaskList = [...tasks]
+    newTaskList.push({name: newTask, priority: priority})
+    // newTaskList.push({name: newTask, priority: event.target.priority.value})
+    setTasks(newTaskList);
+    setNewTask("")
+  }
+
+  const handlePriorityClick = (priorityStr) => {
+    setPriority(priorityStr);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <h1>ToDo List</h1>
+    <hr></hr>
+
+    <form onSubmit={saveNewTask}>
+      <label htmlFor="new-task"> Add New Task</label>
+      <input type = "text" id="new-task" value={newTask} onChange= {handleInputChange}></input>
+      <label htmlFor = "priority">High</label>
+      <input type = "radio" name="priority" value="high" onClick={() => handlePriorityClick("high")}></input>
+      <label htmlFor = "priority">Low</label>
+      <input type = "radio" name="priority" value="low" onClick={() => handlePriorityClick("low")}></input>
+      <input type = "submit" value="Save new task"></input>
+    </form>
+
+    <ul>
+      {taskNodes}
+    </ul>
+
+  </div>
+);
 }
 
 export default App;
